7b6134e09ecf574b9dbbb0533527b6c105adc344
Njoku Okpan <okpan0002@gmail.com>
2026-09-16 16:28:47 +0100

    Use Gmail SMTP port 587

--- backend/src/services/email.service.js
+++ backend/src/services/email.service.js
@@ -6,13 +6,15 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
 
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
   },
+
+  requireTLS: true,
 
   connectionTimeout: 30000,
   greetingTimeout: 30000,
